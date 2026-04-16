6164dfd901a90742d0b2622bd37de23a14842ca0
Peter Binny <peterbinny@gmail.com>
2026-04-16 03:37:58 +0000

    Add workflow to test joke action

--- src/joke.js
+++ src/joke.js
@@ -12,7 +12,21 @@ const options = {
 
 async function getJoke() {
   const res = await request(options);
+  console.log(res)
   return res.joke;
 }
 
 export default getJoke;
+
+/**
+/workspaces/skills-write-javascript-actions (main) $ node src/main.js 
+
+{
+  id: 'baFQZv4w5wc',
+  joke: 'My New Years resolution is to stop leaving things so late.',
+  status: 200
+}
+My New Years resolution is to stop leaving things so late.
+
+::set-output name=joke::My New Years resolution is to stop leaving things so late.
+**/
